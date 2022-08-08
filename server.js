732e26a3aaa2ba95be39900d68aebb48f4b6ce33
Diamond Charlotin <diamond.charlotin@nbcuni.com>
2022-08-08 12:09:03 -0400

    adding post and put

--- server.js
+++ server.js
@@ -21,7 +21,17 @@ const config = {
 
     abcd: 3
 };
-
+var users=[{
+    id:1,
+    name:"diamond",
+    email:"diamondc@hotmail.com"
+},
+{
+    id:2,
+    name:"alex",
+    email:"alex@alex.com"
+}
+]
 
 
 
@@ -56,9 +66,57 @@ app.get('/abcd', (req, res) => {
     res.status(200).send('hello NBCSports');
 
 });
+app.post('/', (req, res) => {
+    let username=req.body.username
+    let password=req.body.password
+    let success=false
+    let message="invalid login"
+    if (username==="diamond" && password==="password123"){
+        success=true
+        message="valid login"
+    }
+    let response={
+        success,message
+    }
+    let statusCode=401
+    if (success){statusCode=200}
+    res.status(statusCode).send(response);
 
+});
+app.get('/users', (req, res) => {
 
+    let userslist=JSON.stringify(users)
 
+    res.status(200).send(userslist);
+
+});
+app.post('/users', (req, res) => {
+
+    let name=req.body.name
+    let email=req.body.email
+if(!name || !email){
+    res.status(403).send()
+}
+let id=users.length+1
+let user={id,name,email}
+users.push(user)
+    res.status(200).send(users);
+
+});
+app.put('/users/:id', (req, res) => {
+
+let id=parseInt(req.params.id)
+let userIndex=users.findIndex(u => u.id===id)  
+if(userIndex===-1) {
+
+    res.status(404).send()
+
+}  
+let name=req.body.name
+    let email=req.body.email
+    users[userIndex]={id,name,email}
+    res.status(200).send(JSON.stringify(users))
+});
 app.listen(config.port, config.host, (e)=> {
 
     if(e) {
